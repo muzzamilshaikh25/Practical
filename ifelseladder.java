e7858c5fded598dcdc16b81a0cc6060bc15bbc13
muzzamilshaikh25 <127470777+muzzamilshaikh25@users.noreply.github.com>
2023-03-17 12:23:18 +0530

    Add files via upload

---
+++ ifelseladder.java
@@ -0,0 +1,22 @@
+class ifelseladder{
+public static void main(String args[]){
+String name="Muzzzz";
+
+if(name=="muzz"){
+System.out.println("Name is muzzz");
+}
+
+else if(name=="MuZzzz"){
+System.out.println("Name is MuZzzz");
+}
+
+else if(name=="Muzzzz"){
+System.out.println("Name is MuZzzz");
+}
+else{
+System.out.println(name);
+
+}
+	}
+
+}
