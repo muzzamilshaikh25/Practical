49ff41e7190fa478749711d67a1bbbb5229b1e52
muzzamilshaikh25 <127470777+muzzamilshaikh25@users.noreply.github.com>
2023-03-17 12:06:15 +0530

    Add files via upload

---
+++ ifelse.java
@@ -0,0 +1,15 @@
+class ifelse{
+public static void main(String args[]){
+int a=5;
+int b=3;
+if(a+b < 7){
+System.out.println("a+b is less than 10:");
+}
+else{
+System.out.println("a+b is less than 10:");
+
+
+}
+
+}
+}
---
+++ simpleif.java
@@ -0,0 +1,10 @@
+class simpleif{
+public static void main(String args[]){
+int a=5;
+int b=3;
+if(a+b < 10){
+System.out.println("a+b is less than 10:");
+}
+
+}
+}
