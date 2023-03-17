785e1eb85c95a2921a719b9719ea33f4135475cd
muzzamilshaikh25 <127470777+muzzamilshaikh25@users.noreply.github.com>
2023-03-17 12:58:28 +0530

    Add files via upload

---
+++ Switch_test.java
@@ -0,0 +1,32 @@
+class Switch_test{
+public static void main(String args[]){
+int a=10;
+switch(a){
+
+case 0:
+System.out.println("Number is 0");
+break;
+
+case 1:
+System.out.println("Number is 1");
+break;
+
+case 2:
+System.out.println("Number is 2");
+break;
+
+case 3:
+System.out.println("Number is 3");
+break;
+
+case 10:
+System.out.println("Number is 10");
+break;
+default:
+System.out.println(a);
+
+}
+
+
+}
+}
