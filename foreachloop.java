01806cdaab42dd4da7c20025b289bc1617d9324a
muzzamilshaikh25 <127470777+muzzamilshaikh25@users.noreply.github.com>
2023-03-20 11:50:31 +0530

    Add files via upload

---
+++ foreachloop.java
@@ -0,0 +1,14 @@
+class foreachloop{
+public static void main(String args[]){
+
+String[] names={"Muzz","Rohit","Amit","Loki"};
+System.out.println("Printing the content of the studentnames\n");
+for(String result:names){
+System.out.println(result);
+}
+
+
+}
+
+
+}
