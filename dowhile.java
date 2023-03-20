a829e91f6e10aee95351c591cc5880b45597b676
muzzamilshaikh25 <127470777+muzzamilshaikh25@users.noreply.github.com>
2023-03-20 13:59:27 +0530

    Add files via upload

---
+++ dowhile.java
@@ -0,0 +1,16 @@
+class dowhile{
+public static void main(String args[]){
+
+int i=0;
+System.out.println("First 10 even numbers:");
+do{
+System.out.println(i);
+i=i+2;
+
+}
+while(i<=10);
+
+
+}
+
+}
---
+++ whileloop.java
@@ -0,0 +1,14 @@
+class whileloop{
+
+public static void main(String args[]){
+
+int i=0;
+System.out.println("The first 10 even numbers:");
+while(i<=10){
+System.out.println(i);
+i=i+2;
+}
+}
+
+
+}
