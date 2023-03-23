d10eb99118e2282945dbdec858a621f18a5e38f2
muzzamilshaikh25 <127470777+muzzamilshaikh25@users.noreply.github.com>
2023-03-23 11:53:03 +0530

    Add files via upload

---
+++ Package_eg.java
@@ -0,0 +1,10 @@
+package package_eg;
+
+public class Package_eg {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hii");
+		System.out.println("First Package is done!!!!");
+	}
+}
