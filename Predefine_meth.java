420ed151d9aa470c5f3c0c0a353558729477ee9e
muzzamilshaikh25 <127470777+muzzamilshaikh25@users.noreply.github.com>
2023-03-27 12:13:50 +0530

    Add files via upload

---
+++ Predefine_meth.java
@@ -0,0 +1,8 @@
+package Method;
+
+public class Predefine_meth {
+	public static void main(String[] args) {
+		System.out.println("The Minimum no is"+Math.min(20, 10));
+	}
+
+}
---
+++ User_defined.java
@@ -0,0 +1,14 @@
+package Method;
+
+public class User_defined {
+	//public static void main(String[] args) {
+		public int add(int a,int b) {
+			return a+b;
+			
+		}
+public static void main(String[] args) {
+	User_defined obj=new User_defined();
+	System.out.println(obj.add (10,35));
+}
+		
+}
