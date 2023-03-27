e4a56cab3fb204466e1cb494f74c64ec7fc04246
muzzamilshaikh25 <127470777+muzzamilshaikh25@users.noreply.github.com>
2023-03-27 12:14:17 +0530

    Add files via upload

---
+++ Object_eg.java
@@ -0,0 +1,14 @@
+package Com;
+
+public class Object_eg {
+	int Sl_no=1;
+	String name="Muzzz";
+	public static void main(String[] args) {
+		Object_eg Obj=new Object_eg();
+		System.out.println(Obj.Sl_no);
+		System.out.println(Obj.name);
+	}
+	
+	
+
+}
---
+++ Student.java
@@ -0,0 +1,42 @@
+package Com;
+
+public class Student {
+	String name="Muzzamil";
+	int age=21;
+	String add="Solapur";
+	
+	String name2="Loki";
+	int age2=21;
+	String add2="Solapur";
+	
+	String name3="Abhi";
+	int age3=21;
+	String add3="Solapur";
+	public static void main(String[] args) {
+		Student Obj=new Student();
+		System.out.println("First Student details:" +Obj.name+","+Obj.age+","+Obj.add);
+		//System.out.println(Obj.name);
+		//System.out.println(Obj.age);
+		//System.out.println(Obj.add);
+		System.out.println("\n");
+		System.out.println("Second Student details:" +Obj.name2+","+Obj.age2+","+Obj.add2);
+		//System.out.println(Obj.name2);
+		//System.out.println(Obj.age2);
+		//System.out.println(Obj.add2);
+		System.out.println("\n");
+		System.out.println("Third Student details:" +Obj.name3+","+Obj.age3+","+Obj.add3);
+		
+		//System.out.println(Obj.name3);
+		//System.out.println(Obj.age3);
+		//System.out.println(Obj.add3);
+		
+
+		
+		
+		
+
+		
+		
+	}
+
+}
