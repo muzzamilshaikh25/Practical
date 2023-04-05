6cf2bc148bc8437295fd791c472f9be47140e267
muzzamilshaikh25 <127470777+muzzamilshaikh25@users.noreply.github.com>
2023-04-05 11:55:43 +0530

    Add files via upload

---
+++ Address.java
@@ -0,0 +1,15 @@
+package Inheritance;
+
+public class Address {
+	String City;
+	String Dist;
+	String Countr;
+	
+	Address(String City,String Dist,String Countr){
+		this.City=City;
+		this.Dist=Dist;
+		this.Countr=Countr;
+		
+	}
+
+}
